441558f2c166ba4329c537ab879a51e826547187
RyanIaro <hei.ryan.3@gmail.com>
2025-11-29 08:59:42 +0300

    chore: react-native-paper deps for design

---
+++ app/auth.tsx
@@ -0,0 +1,20 @@
+import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { Text, TextInput } from 'react-native-paper';
+
+export default function AuthScreen() {
+  return (
+    <KeyboardAvoidingView
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View>
+        <Text>Create account</Text>
+        <TextInput
+        label="Email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        placeholder="example@gmail.com"
+        />
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
