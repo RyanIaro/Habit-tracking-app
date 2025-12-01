568bad2c8ff98ad50d274c8e3f6267b3a1a78628
RyanIaro <hei.ryan.3@gmail.com>
2025-12-01 16:30:48 +0300

    feat: new screens and improved navbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,51 @@
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
     <>
-      <Tabs screenOptions={{tabBarActiveTintColor: "green"}}>
+      <Tabs screenOptions={{
+        headerStyle: { backgroundColor: "#f5f5f5" },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: "#f5f5f5",
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        tabBarActiveTintColor: "#6200ee",
+        tabBarInactiveTintColor: "#666666",
+        }}>
         <Tabs.Screen name="index" options={{
-          title:"Home",
-          tabBarIcon: ({ color, focused }) => {
-            // different icons depending on focused or not
-            return focused ? 
-            <FontAwesome5 name="home" size={24} color={color} /> : <FontAwesome5 name="home" size={24} color="gray" />
-          }
+          title:"Today's habits",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+            name="calendar-today"
+            color={color}
+            size={size}
+            />
+          )
         }}/>
-        <Tabs.Screen name="login" options={{title:"Login"}} />
+        <Tabs.Screen name="streaks" options={{
+          title:"Streaks",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+            name="chart-line"
+            color={color}
+            size={size}
+            />
+          )
+        }}/>
+        <Tabs.Screen name="add-habit" options={{
+          title:"Add habit",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+            name="plus-circle"
+            color={color}
+            size={size}
+            />
+          )
+        }}/>
       </Tabs>
     </>
   );
