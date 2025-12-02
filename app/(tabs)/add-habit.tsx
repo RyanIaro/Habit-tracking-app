b13f0d81eaf1f760a09f582df5ad1100fb64c66a
RyanIaro <hei.ryan.3@gmail.com>
2025-12-02 09:50:35 +0300

    feat: habit creation logic

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,9 +1,100 @@
-import { Text, View } from "react-native";
+import { databaseId, databases, habitsTableId } from "@/lib/appwrite";
+import { useAuth } from "@/lib/auth-context";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { ID } from "react-native-appwrite";
+import { Button, SegmentedButtons, Text, TextInput, useTheme } from "react-native-paper";
+
+const frequencies = ["daily", "weekly", "monthly"];
+
+type Frequency = (typeof frequencies)[number];
 
 export default function AddHabitScreen() {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [frequency, setFrequency] = useState<Frequency>("daily");
+  const [error, setError] = useState<string>("");
+  const { user } = useAuth();
+  const router = useRouter();
+  const theme = useTheme();
+
+  const handleSubmit = async () => {
+    if(!user) return;
+    
+    try {
+      await databases.createRow({
+        databaseId: databaseId,
+        tableId: habitsTableId,
+        rowId: ID.unique(),
+        data: {
+          user_id: user.$id,
+          title,
+          description,
+          frequency,
+          streak_count: 0,
+          last_completed: new Date().toISOString(),
+          creation_date: new Date().toISOString(),
+        }
+      });
+
+      router.back();
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        return;
+      }
+      setError("An error occured while creating the habit")
+    }
+  }
+
   return (
-    <View>
-      <Text>This is the add habit screen</Text>
+    <View style={styles.container}>
+      <TextInput
+      label={"Title"}
+      mode="outlined"
+      style={styles.input}
+      onChangeText={setTitle}
+      />
+      <TextInput
+      label={"Description"}
+      mode="outlined"
+      style={styles.input}
+      onChangeText={setDescription}
+      />
+      <View style={styles.frequencyContainer}>
+        <SegmentedButtons
+        value={frequency}
+        onValueChange={(value) => setFrequency(value as Frequency)}
+        buttons={frequencies.map((freq) => ({
+          value: freq,
+          label: freq.charAt(0).toUpperCase() + freq.slice(1),
+        }))}
+        />
+      </View>
+      <Button
+      mode="contained"
+      disabled={!title || !description}
+      onPress={handleSubmit}
+      >
+        Add habit
+      </Button>
+      {error && <Text style={{color: theme.colors.error}}>{error}</Text>}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center",
+  },
+  input: {
+    marginBottom: 16,
+  },
+  frequencyContainer: {
+    marginBottom: 24,
+  },
+})
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Account, Client } from 'react-native-appwrite';
+import { Account, Client, TablesDB } from 'react-native-appwrite';
 
 export const client = new Client()
   .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
@@ -6,3 +6,7 @@ export const client = new Client()
   .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
 
 export const account = new Account(client);
+export const databases = new TablesDB(client);
+
+export const databaseId = process.env.EXPO_PUBLIC_BD_ID!;
+export const habitsTableId = process.env.EXPO_PUBLIC_HABITS_TABLE_ID!;
