0af032cbdcd7c7e4f5debe17d51db61e99d0ffb9
RyanIaro <hei.ryan.3@gmail.com>
2025-12-14 10:01:28 +0300

    feat: fetching streak data

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -1,9 +1,93 @@
+import { completionsTableId, databaseId, databases, habitsTableId } from "@/lib/appwrite";
+import { useAuth } from "@/lib/auth-context";
+import { Habit, HabitCompletion } from "@/types/database.type";
+import { useCallback, useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { Query } from "react-native-appwrite";
 
 export default function StreaksScreen() {
+  const [habits, setHabits] = useState<Habit[]>([]);
+  const [completed, setCompleted] = useState<HabitCompletion[]>([]);
+    const { user } = useAuth();
+
+  const fetchHabits = useCallback(async () => {
+    try {
+      const response = await databases.listRows({
+        databaseId,
+        tableId: habitsTableId,
+        queries: [Query.equal("user_id", user?.$id ?? "")],
+      });
+      // console.log(response.rows);
+      setHabits(response.rows as Habit[])
+    } catch (error) {
+      console.error(error);
+    }
+  }, [user]);
+
+  const fetchCompletions = useCallback(async () => {
+    try {
+      const response = await databases.listRows({
+        databaseId,
+        tableId: completionsTableId,
+        queries: [Query.equal("user_id", user?.$id ?? "")],
+      });
+      setCompleted(response.rows as HabitCompletion[]);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [user]);
+  
+  useEffect(() => {
+    if (user) {
+      fetchHabits();
+      fetchCompletions();
+    }
+  },[user, fetchHabits, fetchCompletions]);
+
+  interface StreakData {
+    streak: number;
+    bestStreak: number;
+    total: number;
+  }
+
+  const getStreakData = (habitId: string): StreakData => {
+    const habitCompletions = completed?.filter((c) => c.habit_id === habitId)
+    .sort((a, b) => 
+      new Date(a.completed_at).getTime() -
+      new Date(a.completed_at).getTime()
+    );
+
+    if(completed?.length === 0) return {streak: 0, bestStreak: 0, total: 0};
+
+    let streak = 0;
+    let bestStreak = 0;
+    let total = completed.length;
+
+    let lastDate: Date | null = null;
+    let currentStreak = 0;
+
+    habitCompletions?.forEach((c) => {
+      const date = new Date(c.completed_at);
+      if(lastDate) {
+        const diff = date.getTime() - lastDate.getTime() / (1000 * 60 * 60 * 24);
+        if(diff <= 1.5) {
+          currentStreak += 1;
+        } else {
+          currentStreak = 1;
+        }
+      } else {
+        if (currentStreak > bestStreak) bestStreak = currentStreak;
+        streak = currentStreak;
+        lastDate = date;
+      }
+    })
+
+    return {streak, bestStreak, total};
+  };
+
   return (
     <View>
-      <Text>This is the streaks screen</Text>
+      <Text>Habit streaks</Text>
     </View>
   );
 }
